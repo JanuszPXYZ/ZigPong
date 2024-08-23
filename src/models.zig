e2047e0aa28287687fcc298166e91a43935e2129
Janusz Polowczyk <januszpolowczyk19@gmail.com>
2024-08-23 13:02:56 +0200

    Feature: Fully working collision system

--- src/models.zig
+++ src/models.zig
@@ -20,28 +20,63 @@ pub const Ball = struct {
     }
 
     pub fn update(self: *Ball, screenHeight: f32, screenWidth: f32) void {
-        // Adding speed vector to the position means that we're
-        // adding to both coordinates. For now, I'll change this
-        // so that the ball bounces only horizontally. It'll be
-        // easier to test the collisions with paddles
-        self.position.x += self.speed.x;
-        //self.position = rlm.vector2Add(self.position, self.speed);
-        _ = screenHeight;
-        _ = screenWidth;
-        // check for y collisions
-        // if (self.position.y - self.radius <= 0 or self.position.y + self.radius >= screenHeight) {
-        //     self.speed.y *= -1;
-        // }
+        var prng = std.rand.DefaultPrng.init(blk: {
+            var seed: u64 = undefined;
+            std.posix.getrandom(std.mem.asBytes(&seed)) catch |err| {
+                std.debug.print("Error getting random seed: {}\n", .{err});
+            };
+            break :blk seed;
+        });
+        var rand = prng.random();
 
-        // if (self.position.x - self.radius <= 0 or self.position.x + self.radius >= screenWidth) {
-        //     self.speed.x *= -1;
-        //  }
+        // Update position
+        self.position = rlm.vector2Add(self.position, self.speed);
 
-        if (self.position.x - self.radius <= self.paddleLeft.position.x + self.paddleLeft.size.x and (self.position.y >= self.paddleLeft.position.y and self.position.y <= self.paddleLeft.position.y + self.paddleLeft.size.y)) {
-            self.speed.x *= -1;
-        } else if (self.position.x + self.radius >= self.paddleRight.position.x and (self.position.y >= self.paddleRight.position.y and self.position.y <= self.paddleRight.position.y + self.paddleRight.size.y)) {
-            self.speed.x *= -1;
+        // Check for top and bottom collisions
+        if (self.position.y - self.radius <= 0 or self.position.y + self.radius >= screenHeight) {
+            self.speed.y *= -1;
+            // Add a small angle change for variety
+            const angle = (rand.float(f32) * 5 - 2.5) * std.math.pi / 180.0;
+            self.speed = rlm.vector2Rotate(self.speed, angle);
         }
+
+        // Check for paddle collisions
+        if (self.speed.x < 0 and self.position.x - self.radius <= self.paddleLeft.position.x + self.paddleLeft.size.x and
+            self.position.y >= self.paddleLeft.position.y and self.position.y <= self.paddleLeft.position.y + self.paddleLeft.size.y)
+        {
+            self.handlePaddleCollision(self.paddleLeft);
+        } else if (self.speed.x > 0 and self.position.x + self.radius >= self.paddleRight.position.x and
+            self.position.y >= self.paddleRight.position.y and self.position.y <= self.paddleRight.position.y + self.paddleRight.size.y)
+        {
+            self.handlePaddleCollision(self.paddleRight);
+        }
+
+        // Check for scoring (ball goes past paddle)
+        if (self.position.x < 0 or self.position.x > screenWidth) {
+            // Reset ball position and randomize direction
+            self.position = rl.Vector2.init(screenWidth / 2, screenHeight / 2);
+            const randomAngle = (prng.random().float(f32) * 360) * std.math.pi / 180.0;
+            self.speed = rlm.vector2Rotate(rl.Vector2.init(5, 0), randomAngle);
+        }
+    }
+
+    fn handlePaddleCollision(self: *Ball, paddle: *Paddle) void {
+        // Reverse horizontal direction
+        self.speed.x *= -1;
+
+        // Calculate relative impact point
+        const paddleCenter = paddle.position.y + (paddle.size.y / 2);
+        const relativeIntersectY = self.position.y - paddleCenter;
+        const normalizedRelativeIntersectionY = relativeIntersectY / (paddle.size.y / 2);
+        const bounceAngle = -normalizedRelativeIntersectionY * (std.math.pi / 4.0); // Max angle: 45 degrees
+
+        // Calculate new velocity
+        const speed = rlm.vector2Length(self.speed);
+        self.speed.x = speed * std.math.cos(bounceAngle) * std.math.sign(self.speed.x);
+        self.speed.y = speed * -std.math.sin(bounceAngle);
+
+        // Increase speed slightly
+        self.speed = rlm.vector2Scale(self.speed, 1.05);
     }
 
     pub fn draw(self: *Ball) void {
